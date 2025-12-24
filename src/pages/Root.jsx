c382d50f1b0bcd3398b7ac1eb05bcc6bceb4aca5
phantom42 <phantom42@gmail.com>
2025-12-23 20:21:31 -0500

    env vars

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-
+console.log(import.meta.env.MODE);
 export default function Root(){
 	return (
 		<div className="flex items-center h-screen mx-auto bg-blue=500">
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-	port: 5173,
-	proxy: {
-		"/api": {
-			target: "http://localhost:3000",
-			changeOrigin: true
+export default defineConfig(({command, mode}) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+
+		plugins: [react(), tailwindcss()],
+		server: {
+			port: 5173,
+			proxy: {
+				"/api": {
+					target: env.VITE_API_URL,
+					changeOrigin: true
+				}
+			}
 		}
 	}
-  }
 })
