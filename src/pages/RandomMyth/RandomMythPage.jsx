f076cd38c916ef828f03607e39ecafddd72f2dcb
phantom42 <phantom42@gmail.com>
2026-01-03 16:17:27 -0500

    env vars

--- src/pages/RandomMyth/RandomMythPage.jsx
+++ src/pages/RandomMyth/RandomMythPage.jsx
@@ -5,18 +5,9 @@ export default function RandomMythPage() {
 	//const {randomMyth} = useLoaderData() ;
 	const {randomMythList} = useLoaderData();
 	const [mythIndex, setMythIndex] = useState(0);
-	const [autoAdvance, setAutoAdvance] = useState(true);
-	//let mythIndex=0;
-	//const jsonList = JSON.parse(randomMythList);
-	//console.log(jsonList);
-	//console.log(Array.isArray(jsonList));
-	//console.log(jsonList);
-	//const [mythIndex, setMythIndex] = useState(0);
-	//const [mythList, setMythList] = useState(randomMythList);
-
+	const [autoAdvance, setAutoAdvance] = useState(import.meta.env.VITE_AUTOADVANCE);
 	const nextMyth = (e) => {
 		if (e) {
-
 			e.preventDefault();
 		}
 		setMythIndex((mythIndex < randomMythList.length - 1) ? mythIndex + 1 : 0);
@@ -26,12 +17,11 @@ export default function RandomMythPage() {
 		setMythIndex( (mythIndex > 0) ? mythIndex - 1: mythIndex.length - 1);
 	}
 	useEffect(() => {
-		if (!autoAdvance) {
-			return false; 
-		}
 		const timer = setTimeout(()=> {
-			nextMyth()
-		}, 20 * 1000);
+			if (autoAdvance){
+				nextMyth()
+			}
+		}, import.meta.env.VITE_AUTOADVANCE_DELAY_SECONDS * 1000);
 		return () => clearTimeout(timer);
 	});
 
