3ff36a367807e9a51a185795fe200e60cb684c4a
phantom42 <phantom42@gmail.com>
2026-04-25 23:55:47 -0400

    spelling

--- src/pages/Solution/Guardianship.tsx
+++ src/pages/Solution/Guardianship.tsx
@@ -4,7 +4,7 @@ export default function Guardianship() {
 			<header>The alternative that already exists</header>
 			<section className="box">
 				<h2>Permanent Legal Guardianship</h2>
-				<p className="subtext">Already exists as an alternative to plenary adoption, and sidesteps many of its most harmful elements. <em>This should be the solution until all affected parties can provide informed consent</em> when reunifcation is impossible or unsafe and a permanent external home is necessary.</p>
+				<p className="subtext">Already exists as an alternative to plenary adoption, and sidesteps many of its most harmful elements. <em>This should be the solution until all affected parties can provide informed consent</em> when reunification is impossible or unsafe and a permanent external home is necessary.</p>
 			</section>
 		</article>
 	)
