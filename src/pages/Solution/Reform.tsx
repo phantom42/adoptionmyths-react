f2dcdc27c12dc533bd8fd2f518290f4c27c31a7d
phantom42 <phantom42@gmail.com>
2026-04-27 21:57:04 -0400

    tooltips

--- src/pages/Solution/Reform.tsx
+++ src/pages/Solution/Reform.tsx
@@ -3,20 +3,22 @@ export default function Reform() {
 		<article id="reform">
 			<header>Sensible Reform</header>
 			<section>
-				<li>No falsified birth certificates</li>
-				<li>No loss of access to original records</li>
-				<li>No serverance from original families</li>
-				<li>No loss of legal medical history</li>
-				<li>Guaranteed mental health resources</li>
-				<li>No international adoptions</li>
-				<li>No tax breaks for adopting</li>
-				<li>No private for-profit adoption industry</li>
-				<li>Standardized waiting periods after birth</li>
-				<li>Standardized revocation periods</li>
-				<li>No moving parents across state lines</li>
-				<li>No agency solicitation of expectant parents</li>
-				<li>No race/gender-based pricing of children</li>
-				<li>Better vetting & regular wellness checks</li>
+				<li className="has-tooltip" data-tooltip="The names of birth parents are falsified, and locations or dates may also be changed">No falsified birth certificates</li>
+				<li className="has-tooltip" data-tooltip="Only 16 states allow full access to the original, unmodified document">No loss of access to original records</li>
+				<li className="has-tooltip" data-tooltip="While loss of parental rights is sometimes needed, there is no need to be completely legally severed from the entire family">No severance from original families</li>
+				<li className="has-tooltip" data-tooltip="Some genetic testing and cancer screenings require a close relative to have a history of illness. Insurance agencies have denied claims due to lack of legal connection">No loss of legal medical history</li>
+				<li className="has-tooltip" data-tooltip="Familial separation and adoption are a traumatic events that many adoptees struggle with into adulthood">Guaranteed mental health resources</li>
+				<li className="has-tooltip" data-tooltip="Pre-birth matching places undue pressure on expectant parents and increases expectations of hopeful parents">No pre-birth matching</li>
+				<li className="has-tooltip" data-tooltip="Adoption agencies and hopeful parents weaponize the complex legal system to pressure expectant parents">Provided 3rd-party legal advocate for expectant parents</li>
+				<li className="has-tooltip" data-tooltip="There is an immense amount of corruption in international adoption. From ">No international adoptions</li>
+				<li className="has-tooltip" data-tooltip="Tax-breaks and refunds contribute to the commodification of children and directs financial aid to the wrong people">No tax breaks for adopting</li>
+				<li className="has-tooltip" data-tooltip="In order to acquire infants, the $30 billion industry preys on vulnerable people in crisis and commodifies children">No private for-profit adoption industry</li>
+				<li className="has-tooltip" data-tooltip="Some states allow parents to relinquish immediately after birth, when many new parents are still on painkillers and dealing with post-partum ">Standardized waiting periods after birth</li>
+				<li className="has-tooltip" data-tooltip="Revocation periods vary from state-to-state. Anywhere between no period and 45 days">Standardized revocation periods</li>
+				<li className="has-tooltip" data-tooltip="Agencies move pregnant people to states where they have no local support system and where the revocation periods and waiting periods favor hopeful parents">No moving parents across state lines</li>
+				<li className="has-tooltip" data-tooltip="Agencies and hopeful parents actively approach expectant parents who may have no intention to relinquish in places like hospitals, churches, stores, or on the internet">No solicitation of expectant parents</li>
+				<li className="has-tooltip" data-tooltip="Some agencies vary price of children based on race, age, gender, health of the child, and expectations of expectant parents. On average, white children cost 2x as much as Black children">No race/gender-based pricing of children</li>
+				<li className="has-tooltip" data-tooltip="Home studies and background checks are not thorough enough and there is no check that the child is safe and thriving after adoption">Better vetting & regular wellness checks</li>
 			</section>
 		</article>
 	)
