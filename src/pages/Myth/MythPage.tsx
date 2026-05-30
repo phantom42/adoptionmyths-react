251995d2448d9c0aaa766c13010e8cd61242a729
phantom42 <phantom42@gmail.com>
2026-05-30 15:34:07 -0400

    updated links to new site

--- src/pages/Myth/MythPage.tsx
+++ src/pages/Myth/MythPage.tsx
@@ -13,8 +13,8 @@ export default function MythPage() {
 					{singleMyth &&
 						<DebunkedMyth debunkedMyth={singleMyth} />
 					}
-					<Link to="/the-answer/" className="solution-link italic font-bold cursor-pointer link">
-						<h1 className="font-serif text-3xl">So What's <span className="text-highlight-500 italic">The Answer?</span> <span className="answer-link"> The Answers Already Exist →</span></h1>
+					<Link to="/the-solution/" className="solution-link italic font-bold cursor-pointer link">
+						<h1 className="font-serif text-3xl">So What's <span className="text-highlight-500 italic">The Solution?</span> <span className="answer-link"> The Answers Already Exist →</span></h1>
 					</Link>
 				</div>
 			</div>
--- src/pages/Solution/Headline.tsx
+++ src/pages/Solution/Headline.tsx
@@ -1,7 +1,7 @@
 export default function Headline() {
 	return (
 		<section id="headline">
-			<h1 className="font-serif text-3xl">So What's <span className="text-highlight-500 italic">The Answer?</span></h1>
+			<h1 className="font-serif text-3xl">So What's <span className="text-highlight-500 italic">The Solution?</span></h1>
 			<p className=" text-main-500">The adoption system must be rebuilt from the ground up - centered on the child, not the industry or adoptive parents.</p>
 		</section>
 	)
